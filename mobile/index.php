d695aeaa2574dcb19d4a077d9cf87e0e6684b6fd
lifulforever <layer_co@naver.com>
2018-05-02 17:19:37 +0900

    가비아 모바일 도메인
    가비아 모바일 도메인

---
+++ mobile/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+<title>Untitled Document</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width Initial-scale=1.0, minimum-scale=1.0, user-scalable=yes">
+
+<!--favicon icon-->
+<link rel="shortcut icon" href="image/favicon.ico">
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="image/apple_touch_icon_144x144.png">
+<link rel="apple-touch-icon-precomposed" sizes="120x120" href="image/apple_touch_icon_120x120.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="image/apple_touch_icon_114x114.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="image/apple_touch_icon_72x72.png">
+<link rel="apple-touch-icon-precomposed" sizes="57x57" href="image/apple_touch_icon_57x57.png">
+
+<!--WEB FONT & WEB FONTS ICON-->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="../font/notosanskr.css"/>
+<link rel="stylesheet" type="text/css" href="../font/nanumgothic.css"/>
+<link href="https://cdn.iconmonstr.com/1.1.0/css/iconmonstr-iconic-font.min.css" rel="stylesheet"/>
+
+<!--DESIGN CSS-->
+<link rel="stylesheet" href="../css/animate.css">
+<link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+<header class="header_gn_pc">
+    <div class="l_wrapper">
+        <nav>
+            <main><a href="#0"><img src="logo" alt="로고"></a></main>
+            <div>
+
+            </div>
+            <ul>
+                <li><a href="#0"></a></li>
+
+            </ul>
+        </nav>
+    </div>
+</header>
+
+<section></section>
+<aside></aside>
+
+<footer class="footer_global_pc">
+    <div class="l_wrapper">
+
+    </div>
+</footer>
+<!-- jQuery -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.1/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+</body>
+</html>
